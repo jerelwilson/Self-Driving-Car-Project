36a2c9c49697d8c2e60c4e21b17b1fea80f79179
Jerel <Jknight09@live.com>
2023-08-04 16:48:16 -0400

    Third Commit

--- README.txt
+++ README.txt
@@ -2,6 +2,6 @@ This was a project I did on Codeacademy, The idea is simple: create a vehicle th
 as a human driver would, but the implementation is fraught with obstacles. To accomplish this, you’ll use your knowledge
 of TypeScript object types to apply types to a variety of classes, properties, and methods.
 
-I used Typescript, JSON and Javascript
+I used Typescript, JSON and Javascript.
 
 This project was done by Jerel Wilson
--- computer-vision.js
+++ computer-vision.js
@@ -1,7 +1,11 @@
-export function getObstacleEvents() {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getObstacleEvents = void 0;
+function getObstacleEvents() {
     const coinFlip = Boolean(Math.random() > 0.5);
-    return { 
-      'ObstacleLeft': coinFlip, 
-      'ObstacleRight': !coinFlip 
+    return {
+        'ObstacleLeft': coinFlip,
+        'ObstacleRight': !coinFlip
     };
-  }
+}
+exports.getObstacleEvents = getObstacleEvents;
